f2dac877d80f88a582167c40f2d1fa6c6bd412d8
teste <teste@10.32.175.176>
2018-05-14 15:24:10 -0300

    Primeira configuração com firefox

---
+++ helloworld/src/main/java/br/org/dbserver/helloworld/testcase/LoginTestCase.java
@@ -0,0 +1,37 @@
+package br.org.dbserver.helloworld.testcase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTestCase {
+	private WebDriver driver;
+
+	@Before
+	public void setUp() {
+		WebDriverManager.firefoxdriver().setup();
+		this.driver = new FirefoxDriver();
+
+		this.driver.get("https://virtuemart.net/");
+
+
+
+	}
+
+	@Test
+	public void testMain() {
+
+	}
+
+	@After
+	public void tearDown() {
+		this.driver.quit();				
+	}
+
+
+
+}
